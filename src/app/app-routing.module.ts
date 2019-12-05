73ff674c6ecb8992311b399a4ad52151394793a1
esanchez <esanchez@beesoftware.mx>
2019-12-05 10:13:07 -0600

    first pager

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectsDetailComponent } from './projects/projects-detail/projects-detail.component';
+import { StudioComponent } from './studio/studio.component';
+import { TeamComponent } from './studio/team/team.component';
+import { TeamDetailComponent } from './studio/team/team-detail/team-detail.component';
+import { CollaboratorsComponent } from './studio/collaborators/collaborators.component';
+import { BooksComponent } from './publications/books/books.component';
+import { ArticlesComponent } from './publications/articles/articles.component';
+import { VideoComponent } from './video/video.component';
+import { ContactComponent } from './contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'projects', component: ProjectsComponent, children: [
+      { path: 'project-detail/:id', component: ProjectsDetailComponent },
+    ]
+  },
+  { path: 'studio', component: StudioComponent },
+  {
+    path: 'team', component: TeamComponent, children: [
+      { path: 'team-detail/:id', component: TeamDetailComponent },
+    ]
+  },
+  { path: 'collaborators', component: CollaboratorsComponent },
+  { path: 'books', component: BooksComponent },
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'video', component: VideoComponent },
+  { path: 'contact', component: ContactComponent },
+
+
+  { path: '**', redirectTo: '' }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  public config: SwiperConfigInterface = {
+    a11y: true,
+    direction: 'horizontal',
+    slidesPerView: 1,
+    keyboard: false,
+    mousewheel: false,
+    scrollbar: false,
+    navigation: false,
+    pagination: false,
+    autoplay: {
+      delay: 5000,
+    },
+  };
 
   constructor() { }
 
